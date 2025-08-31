1561b130aa4f5d1046e258a0ee369a91574c0e58
NazmulHasan18 <nazmul182218@gmail.com>
2025-08-31 14:18:16 +0600

    test commit

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -9,99 +9,99 @@ import { usePathname } from "next/navigation";
 import clsx from "clsx";
 
 const Navbar = () => {
-   const navItems = ["About", "Services", "Contact"];
-   const [open, setOpen] = useState(false);
-   const path = usePathname();
+  const navItems = ["About", "Services", "Contact"];
+  const [open, setOpen] = useState(false);
+  const path = usePathname();
 
-   return (
-      <header
-         className={`w-full bg-white bg-opacity-60 bg-[url('/images/navbg.jpg')] bg-cover bg-center bg-blend-soft-light`}
-      >
-         <nav className="container mx-auto flex justify-between items-center py-4 px-6">
-            {/* Logo */}
-            <Link href="/" className="text-2xl font-bold text-blue-600 hover:text-blue-500">
-               MediBridge
+  return (
+    <header
+      className={`w-full bg-white bg-opacity-60 bg-[url('/images/navbg.jpg')] bg-cover bg-center bg-blend-soft-light`}
+    >
+      <nav className="container mx-auto flex justify-between items-center py-4 px-6">
+        {/* Logo */}
+        <Link href="/" className="text-2xl font-bold text-blue-600 hover:text-blue-500">
+          MediBridge
+        </Link>
+
+        {/* Desktop Menu */}
+        <ul className="hidden md:flex space-x-6 text-gray-700">
+          <li>
+            <Link
+              href={`/`}
+              className={clsx(
+                "hover:text-blue-600 font-medium text-black",
+                path === "/" ? "underline text-primary" : "bg-none"
+              )}
+            >
+              Home
             </Link>
+          </li>
+          {navItems.map((item, i) => (
+            <li key={i}>
+              <Link
+                href={`/${item.toLowerCase()}`}
+                className={clsx(
+                  "hover:text-blue-600 font-medium text-black",
+                  path === "/" + item.toLowerCase() ? "underline text-primary" : "bg-none"
+                )}
+              >
+                {item}
+              </Link>
+            </li>
+          ))}
+        </ul>
 
-            {/* Desktop Menu */}
-            <ul className="hidden md:flex space-x-6 text-gray-700">
-               <li>
-                  <Link
-                     href={`/`}
-                     className={clsx(
-                        "hover:text-blue-600 font-medium text-black",
-                        path === "/" ? "underline text-primary" : "bg-none"
-                     )}
-                  >
-                     Home
-                  </Link>
-               </li>
-               {navItems.map((item, i) => (
-                  <li key={i}>
-                     <Link
-                        href={`/${item.toLowerCase()}`}
-                        className={clsx(
-                           "hover:text-blue-600 font-medium text-black",
-                           path === "/" + item.toLowerCase() ? "underline text-primary" : "bg-none"
-                        )}
-                     >
-                        {item}
-                     </Link>
-                  </li>
-               ))}
-            </ul>
+        {/* Auth Buttons */}
+        <div className="hidden md:flex space-x-4">
+          <Link href="/login">
+            <Button variant="outline">Sign In</Button>
+          </Link>
+          <Link href="/sign-up">
+            <Button>Sign Up</Button>
+          </Link>
+        </div>
 
-            {/* Auth Buttons */}
-            <div className="hidden md:flex space-x-4">
-               <Link href="/login">
-                  <Button variant="outline">Sign In</Button>
-               </Link>
-               <Link href="/sign-up">
-                  <Button>Sign Up</Button>
-               </Link>
-            </div>
-
-            {/* Mobile Menu Button */}
-            <Sheet open={open} onOpenChange={setOpen}>
-               <SheetTrigger asChild>
-                  <Button variant="ghost" size="icon" className="md:hidden" aria-label="Open menu">
-                     <Menu className="h-6 w-6" />
-                  </Button>
-               </SheetTrigger>
-               <SheetContent title="Navigators" side="left" className="p-6">
-                  <SheetTitle className="text-2xl font-bold text-blue-600 ">MediBridge</SheetTitle>
-                  <nav className="flex flex-col space-y-4 text-lg">
-                     <Link
-                        href={`/`}
-                        onClick={() => setOpen(false)}
-                        className="p-2 rounded-md transition-all duration-200 active:bg-gray-100 hover:bg-blue-100 hover:text-gray-800"
-                     >
-                        Home
-                     </Link>
-                     {navItems.map((item, i) => (
-                        <Link
-                           key={i}
-                           href={`/${item.toLowerCase()}`}
-                           onClick={() => setOpen(false)}
-                           className="p-2 rounded-md transition-all duration-200 active:bg-gray-100 hover:bg-blue-100 hover:text-gray-800"
-                        >
-                           {item}
-                        </Link>
-                     ))}
-                     <Link href="/login">
-                        <Button variant="outline" className="w-full active:scale-95">
-                           Sign In
-                        </Button>
-                     </Link>
-                     <Link href="/sign-up">
-                        <Button className="w-full active:scale-95">Sign Up</Button>
-                     </Link>
-                  </nav>
-               </SheetContent>
-            </Sheet>
-         </nav>
-      </header>
-   );
+        {/* Mobile Menu Button test */}
+        <Sheet open={open} onOpenChange={setOpen}>
+          <SheetTrigger asChild>
+            <Button variant="ghost" size="icon" className="md:hidden" aria-label="Open menu">
+              <Menu className="h-6 w-6" />
+            </Button>
+          </SheetTrigger>
+          <SheetContent title="Navigators" side="left" className="p-6">
+            <SheetTitle className="text-2xl font-bold text-blue-600 ">MediBridge</SheetTitle>
+            <nav className="flex flex-col space-y-4 text-lg">
+              <Link
+                href={`/`}
+                onClick={() => setOpen(false)}
+                className="p-2 rounded-md transition-all duration-200 active:bg-gray-100 hover:bg-blue-100 hover:text-gray-800"
+              >
+                Home
+              </Link>
+              {navItems.map((item, i) => (
+                <Link
+                  key={i}
+                  href={`/${item.toLowerCase()}`}
+                  onClick={() => setOpen(false)}
+                  className="p-2 rounded-md transition-all duration-200 active:bg-gray-100 hover:bg-blue-100 hover:text-gray-800"
+                >
+                  {item}
+                </Link>
+              ))}
+              <Link href="/login">
+                <Button variant="outline" className="w-full active:scale-95">
+                  Sign In
+                </Button>
+              </Link>
+              <Link href="/sign-up">
+                <Button className="w-full active:scale-95">Sign Up</Button>
+              </Link>
+            </nav>
+          </SheetContent>
+        </Sheet>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
